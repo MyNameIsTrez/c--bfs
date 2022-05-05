cf0bff21130a420e5f2379dc98afce3eec3b874b
MyNameIsTrez <sbos@student.codam.nl>
2022-05-05 18:10:03 +0200

    Move grid code into its own Grid class.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 // #include <algorithm>
 #include <queue>
 
+
+
 struct Node
 {
     int x;
     int y;
     friend std::ostream& operator<<(std::ostream& os, const Node& node);
 };
+
+
+std::ostream& operator<<(std::ostream& os, const Node& node)
+{
+    os << "x: " << node.x << ", y: " << node.y;
+    return os;
+}
+
+
 
 struct NeighborNodes
 {
@@ -26,22 +37,78 @@ struct NeighborNodes
     }
 };
 
-std::ostream& operator<<(std::ostream& os, const Node& node)
+
+
+class Grid
 {
-    os << "x: " << node.x << ", y: " << node.y;
-    return os;
+    void print_column_indices(std::string::size_type max_row_index_length);
+    void print_row_indices(std::string::size_type max_row_index_length);
+public:
+    int width;
+    int height;
+
+    std::vector<std::vector<char>> grid;
+
+    void print(void);
+};
+
+
+void Grid::print(void)
+{
+    std::string::size_type max_row_index_length = std::to_string(grid.size()).length();
+
+    print_column_indices(max_row_index_length);
+    print_row_indices(max_row_index_length);
 }
+
+
+void Grid::print_column_indices(std::string::size_type max_row_index_length)
+{
+    std::cerr << std::string(max_row_index_length, ' ');
+
+    std::vector<char>::size_type column_count = grid[0].size();
+
+    std::string::size_type max_column_index_length = std::to_string(column_count).length();
+
+    for (int column_index = 0; column_index < column_count; column_index++)
+    {
+        std::cerr << column_index % 10;
+    }
+
+    std::cerr << std::endl;
+}
+
+
+void Grid::print_row_indices(std::string::size_type max_row_index_length)
+{
+    for (int row_index = 0; row_index < grid.size(); row_index++)
+    {
+        std::vector<char> row = grid[row_index];
+
+        std::string::size_type row_index_length = std::to_string(row_index).length();
+
+        std::cerr << std::string(max_row_index_length - row_index_length, ' ');
+
+        std::cerr << row_index;
+
+        for (char cell : row)
+        {
+            std::cerr << cell;
+        }
+        std::cerr << std::endl;
+    }
+}
+
+
 
 class Game
 {
-    int width;
-    int height;
     int alarm_length;
 
     int player_x;
     int player_y;
 
-    std::vector<std::vector<char>> grid;
+    Grid grid;
 
     void init_parse(void);
 
@@ -54,41 +121,42 @@ class Game
 
     bool node_is_out_of_bounds(Node node);
     bool node_is_wall(Node node);
-
-    void print_grid(void);
-    void print_grid_column_indices(std::string::size_type max_row_index_length);
-    void print_grid_row_indices(std::string::size_type max_row_index_length);
 public:
     Game(void);
     void run(void);
 };
+
 
 Game::Game(void)
 {
     init_parse();
 }
 
+
 void Game::init_parse(void)
 {
-    std::cin >> height >> width >> alarm_length; std::cin.ignore();
+    std::cin >> grid.height >> grid.width >> alarm_length; std::cin.ignore();
 }
+
 
 void Game::run(void)
 {
     while (1)
     {
         parse();
-        print_grid();
+        grid.print();
         const std::vector<Node> path = bfs();
         break;
     }
 }
+
 
 void Game::parse(void)
 {
     parse_player_position();
     parse_grid();
 }
+
 
 std::vector<Node> Game::bfs(void)
 {
@@ -125,6 +193,7 @@ std::vector<Node> Game::bfs(void)
     }
 }
 
+
 NeighborNodes Game::get_neighbor_nodes(Node node)
 {
     NeighborNodes neighbor_nodes;
@@ -147,76 +216,38 @@ NeighborNodes Game::get_neighbor_nodes(Node node)
     return neighbor_nodes;
 }
 
+
 bool Game::node_is_out_of_bounds(Node node)
 {
     return (
-        node.x <= -1 || node.x >= width ||
-        node.y <= -1 || node.y >= height
+        node.x <= -1 || node.x >= grid.width ||
+        node.y <= -1 || node.y >= grid.height
     );
 }
 
+
 bool Game::node_is_wall(Node node)
 {
-    return grid[node.y][node.x] == '#';
+    return grid.grid[node.y][node.x] == '#';
 }
+
 
 void Game::parse_player_position(void)
 {
     std::cin >> player_y >> player_x; std::cin.ignore();
 }
 
+
 void Game::parse_grid(void)
 {
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < grid.height; i++) {
         std::string row;
         std::cin >> row; std::cin.ignore();
-        grid.push_back(std::vector<char>(row.begin(), row.end()));
+        grid.grid.push_back(std::vector<char>(row.begin(), row.end()));
     }
 }
 
-void Game::print_grid(void)
-{
-    std::string::size_type max_row_index_length = std::to_string(grid.size()).length();
 
-    print_grid_column_indices(max_row_index_length);
-    print_grid_row_indices(max_row_index_length);
-}
-
-void Game::print_grid_column_indices(std::string::size_type max_row_index_length)
-{
-    std::cerr << std::string(max_row_index_length, ' ');
-
-    std::vector<char>::size_type column_count = grid[0].size();
-
-    std::string::size_type max_column_index_length = std::to_string(column_count).length();
-
-    for (int column_index = 0; column_index < column_count; column_index++)
-    {
-        std::cerr << column_index % 10;
-    }
-
-    std::cerr << std::endl;
-}
-
-void Game::print_grid_row_indices(std::string::size_type max_row_index_length)
-{
-    for (int row_index = 0; row_index < grid.size(); row_index++)
-    {
-        std::vector<char> row = grid[row_index];
-
-        std::string::size_type row_index_length = std::to_string(row_index).length();
-
-        std::cerr << std::string(max_row_index_length - row_index_length, ' ');
-
-        std::cerr << row_index;
-
-        for (char cell : row)
-        {
-            std::cerr << cell;
-        }
-        std::cerr << std::endl;
-    }
-}
 
 int main()
 {
